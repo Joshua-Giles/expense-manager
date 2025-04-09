e52bfc33a074c08f6db1bc65b8d6f95c4ac21562
jagiles <jagiles@ncsu.edu>
2025-04-09 16:07:32 -0400

    Initial React frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Budget Tracker</h1>
+      <p>Welcome to your personal expense manager!</p>
+    </div>
+  );
+}
+
+export default App;
